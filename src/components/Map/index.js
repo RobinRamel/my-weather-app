650f0b45111c538232b4eef7e3a83dceedb19f07
Robin-R <ramel.robin@gmail.com>
2022-11-03 09:58:50 +0100

    ajout de react-leaflet pour la map

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,17 +1,43 @@
-import PropTypes from 'prop-types';
 
-import './style.scss';
+import { MapContainer, TileLayer } from 'react-leaflet'
+import { useMap } from 'react-leaflet/hooks'
+import './style.scss'
+import 'leaflet/dist/leaflet.css'
+import { useSelector } from 'react-redux';
 
+// Children Component to MAP to be able to change the center of the map on update
+// cause we can't access context / hooks of react leaflet only in descendant of map container
+function ChangeView({ center }) {
+  const map = useMap();
+  map.setView(center);
+  return null;
+}
+
+// Main MAP Component 
 function Map() {
+  const position = useSelector(state => state.localisation.coord)
+
   return (
-    <div className="localisation">
-             
+    <div className="localisation" id="map">
+      <MapContainer center={[position.lat, position.long]} zoom={10} scrollWheelZoom={true}>
+        <ChangeView center={[position.lat, position.long]} /> 
+        
+        {/* Layer to add on map for later Updates if needed  */}
+        {/* <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=8515322bfc58d345e1e14f44a6c2332e`}
+          zIndex={2}
+        /> */}
+
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          zIndex={0}
+        />
+      </MapContainer>
     </div>
   );
 }
 
-Map.propTypes = {
-
-};
 
 export default Map;
--- src/selectors/localStorage.js
+++ src/selectors/localStorage.js
@@ -1,20 +1,15 @@
 
 export const addCityToLocalStorage = ({lat, long, cityName}) => {
     const retrieveLocalStorage = JSON.parse( window.localStorage.getItem('myweatherapp-city-list') )
-    console.log("retrieve localsto : ", retrieveLocalStorage)
-    console.log("data in func : ", lat, long, cityName)
+
     // if we have localstorage 
     if(retrieveLocalStorage) {
         const isCityAlreadyIn = retrieveLocalStorage.find(
             element => element.cityName === cityName 
             )
-            console.log(isCityAlreadyIn)
              
         // we check if the city dont already exist 
         if(!isCityAlreadyIn) {
-            console.log("city isnt in it ")
-            // add
-            console.log('test of mutating array for local : ', [...retrieveLocalStorage, {lat, long, cityName}])
             window.localStorage.setItem(
                 'myweatherapp-city-list',
                 JSON.stringify(
